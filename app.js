be2fee5cb5078ac8fc68abfbbc32db9a1f27ede5
Trillion_Impulse <tkdduq2897@naver.com>
2025-04-13 20:21:39 +0900

    feat(login): 사용자 입력 후 폼 숨기기, 인사 메세지 출력 추가
    - form 제출 시 기본 동작 방지 (preventDefault)
    - form 제출 시 #login-form에 hidden 클래스를 추가하여 숨김 처리
    - 입력된 사용자 이름을 추출하여 greeting 영역에 표시
    - greeting 영역은 hidden 클래스를 제거하여 화면에 표시

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginFormInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function loginButtonClick() {
+const HIDDEN_CLASSNAME = "hidden";
+
+function loginFormSubmit(event) {
+    event.preventDefault();
     const userName = loginFormInput.value;
-    console.log(userName);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
+loginForm.addEventListener("submit", loginFormSubmit);
