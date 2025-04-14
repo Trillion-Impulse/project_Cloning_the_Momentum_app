8332e4e4ed0f3ef6ec852d9832411fd4990b71e8
Trillion_Impulse <tkdduq2897@naver.com>
2025-04-14 22:08:55 +0900

    feat(login): userName을 상수로 추가 및 인사 기능 함수 추가
    - userName 키를 상수로 추가
    - 인사 메세지 출력 함수 paintGreetings 추가
    - localStorage에 저장된 userName의 유무에 따른 기능
        - 유: 자동으로 인사 메세지 출력
        - 무: #login-form 표시

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginFormInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
 function loginFormSubmit(event) {
     event.preventDefault();
     const userName = loginFormInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem("userName", userName);
-    greeting.innerText = `Hello ${userName}`;
+    paintGreetings(userName);
+}
+
+function paintGreetings(name) {
+    greeting.innerText = `Hello ${name}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", loginFormSubmit);
+const savedUsername = localStorage.getItem("userName");
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", loginFormSubmit);
+}
+else {
+    paintGreetings(savedUsername);
+}
