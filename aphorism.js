84f567a2eef6fbcdcb609e490a40ae060228ab27
Trillion_Impulse <tkdduq2897@naver.com>
2025-04-16 20:43:35 +0900

    feat(aphorism/aphorism.js): 명언 배열 추가
    - 명언, 작가 포함한 배열 추가

---
+++ aphorism.js
@@ -0,0 +1,22 @@
+const aphorism = [
+    {
+        aphorism: "That which does not kill us makes us stronger",
+        author: "Friedrich Nietzsche"
+    },
+    {
+        aphorism: "Only those who stand alone can truly go far",
+        author: "Yeop"
+    },
+    {
+        aphorism: "Since the very character of law is order, or constancy, it follows that all that happens will repeat and can be predicted if we know the law",
+        author: "Ralph Nelson Elliott"
+    },
+    {
+        aphorism: "In the middle of every difficulty lies opportunity",
+        author: "Albert Einstein"
+    },
+    {
+        aphorism: "If you can dream it, you can do it",
+        author: "Walt Disney"
+    }
+]
