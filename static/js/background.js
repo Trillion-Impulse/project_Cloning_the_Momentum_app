1236c2c905d869afc705c524815372bfa0f19d71
Trillion_Impulse <tkdduq2897@naver.com>
2025-05-20 21:46:23 +0900

    refactor(all): Flask 연동을 위해 디렉토리 재구성 및 파일 경로 수정
    project_Cloning_the_Momentum_app/
    - app.py
    - templates/
        - index.html
            - app, clock, aphorism, background, todo, config, weather 파일을 Flask 방식에 맞도록 경로 수정
    - static/
        - style.css
        - aphorism.js
        - app.js
        - background.js
            - 이미지 경로 절대 경로로 수정
        - clock.js
        - todo.js
        - weather.js

--- static/js/background.js
+++ static/js/background.js
@@ -2,5 +2,5 @@ const images = ["0.png","1.png"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-document.body.style.backgroundImage = `url('${chosenImage}')`;
+document.body.style.backgroundImage = `url('/static/images/${chosenImage}')`;
 document.body.style.backgroundSize = "cover";
